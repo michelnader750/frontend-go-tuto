33c689dd7fc642115d25121b8889d443cb50dacd
Michel Nader <michelnader750@hotmail.com>
2024-04-01 14:38:33 -0300

    add homepage-carousel

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,32 @@
 <template>
   <NavBar></NavBar>
+  <div class="container">
+    <div class="row">
+      <div class="col-12 d-flex justify-content-center"></div>
+      <HomepageCarousel></HomepageCarousel>
+    </div>
+    <div class="row text-start">
+      <div class="col-md-2 col-6 border text-primary">
+        Text 1
+      </div>
+      <div class="col-md-8 col-6 border">
+        Text 2
+      </div>
+      <div class="col-md-2 col-12 border">
+        Text 3
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 import NavBar from './components/NavBar.vue';
+import HomepageCarousel from "./components/HomepageCarousel.vue";
 
 export default {
   name: 'App',
   components: {
+    HomepageCarousel,
     NavBar
   }
 }
---
+++ src/components/HomepageCarousel.vue
@@ -0,0 +1,41 @@
+<template>
+    <div id="carouselExample" class="carousel slide carousel-custom">
+        <div class="carousel-inner">
+          <div class="carousel-item">
+            <img src="@/assets/cat2.webp" class="d-block w-100" alt="...">
+          </div>
+          <div class="carousel-item">
+            <img src="@/assets/cat2.webp" class="d-block w-100" alt="...">
+          </div>
+          <div class="carousel-item">
+            <img src="@/assets/cat2.webp" class="d-block w-100" alt="...">
+          </div>
+          <div class="carousel-item">
+            <img src="@/assets/cat2.webp" class="d-block w-100" alt="...">
+          </div>
+          <div class="carousel-item active">
+            <img src="@/assets/boat.jpeg" class="d-block w-100" alt="...">
+          </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Next</span>
+        </button>
+      </div>
+</template>
+
+<script>
+export default {
+    name: "HomepageCarousel"
+}
+</script>
+
+<style>
+.carousel-custom {
+  max-width: 600px;
+}
+</style>
